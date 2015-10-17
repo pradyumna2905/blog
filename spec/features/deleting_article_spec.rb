11773e7ba300b630e20e0259f35ff91a34cb7290
Pradyumna <pradyumna2905@gmail.com>
2015-10-16 17:58:19 -0700

    link articles to users

--- spec/features/deleting_article_spec.rb
+++ spec/features/deleting_article_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.feature "Deleting an article" do
    before do
-      @article = Article.create(title: "First article", body: "body of first article") 
+     @john = User.create(email: "john@example.com", password: "password")
+     login_as(@john)
+      @article = Article.create(title: "First article", body: "body of first article", user: @john) 
    end
-   
+
    scenario "A user deletes an article" do
-   
+
        visit "/"
        click_link @article.title
        click_link "Delete Article"
-       
+
        expect(page).to have_content("Article has successfully been deleted")
        expect(page.current_path).to eq(articles_path)
    end
--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -1,34 +1,36 @@
 require "rails_helper"
 
-RSpec.feature "Editing an Article" do 
+RSpec.feature "Editing an Article" do
    before do
-    @article = Article.create(title: "This is the first article", body: "This is the body of the article") 
+    @john = User.create(email: "john@example.com", password: "password")
+    login_as(@john)
+    @article = Article.create(title: "This is the first article", body: "This is the body of the article", user: @john)
    end
-   
+
    scenario "A user updates an article" do
       visit "/"
-      
+
       click_link @article.title
       click_link "Edit Article"
-      
+
       fill_in "Title", with: "Updated article"
       fill_in "Body", with: "Updated body of article"
       click_button "Update Article"
-      
+
       expect(page).to have_content("Article has been successfully updated")
       expect(page.current_path).to eq(article_path(@article))
    end
-   
+
    scenario "A user fails to update an article" do
        visit "/"
-      
+
       click_link @article.title
       click_link "Edit Article"
-      
+
       fill_in "Title", with: ""
       fill_in "Body", with: "Updated body of article"
       click_button "Update Article"
-      
+
       expect(page).to have_content("Article cannot be updated")
       expect(page.current_path).to eq(article_path(@article))
    end
