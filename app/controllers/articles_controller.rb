3d6b7cb16ba208a4b7674f659d8111a07f84a80f
Pradyumna <pradyumna2905@gmail.com>
2015-10-16 17:28:57 -0700

    Stylize devise default messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_filter :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
-  
-  def create 
+
+  def create
     @article = Article.new(article_params)
-    
+
     if @article.save
       flash[:success] = "Article has been created"
       redirect_to articles_path
@@ -19,38 +20,38 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
   end
-  
-  def update 
+
+  def update
     if @article.update(article_params)
       flash[:success] = "Article has been successfully updated"
       redirect_to @article
-    else 
+    else
       flash.now[:danger] = "Article cannot be updated"
       render :edit
     end
   end
-  
+
   def show
   end
-  
-  def destroy 
+
+  def destroy
     if @article.destroy
       flash[:danger] = "Article has successfully been deleted"
       redirect_to articles_path
     end
   end
-  
+
     private
-    
+
       def article_params
         params.require(:article).permit(:title, :body)
       end
-      
+
       def set_article
         @article = Article.find(params[:id])
       end
-  
+
 end
