8b55976fa36fe6fe7a866a9583e5eca905010764
Pradyumna <pradyumna2905@gmail.com>
2015-10-16 18:32:10 -0700

    Restrict access to certain features in rspec tests and also UI

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature "Listing Articles" do 
-   
+RSpec.feature "Listing Articles" do
+
    before do
-    @article1 = Article.create(title: "First article", body: "The is totally random") 
-    @article2 = Article.create(title: "Second article", body: "The is even more random") 
+    @article1 = Article.create(title: "First article", body: "The is totally random")
+    @article2 = Article.create(title: "Second article", body: "The is even more random")
    end
-    
+
     scenario "List all articles" do
         visit "/"
-        
+
         expect(page).to have_content(@article1.title)
         expect(page).to have_content(@article1.body)
-        
+
         expect(page).to have_content(@article2.title)
         expect(page).to have_content(@article2.title)
-        
-        expect(page).to have_link(@article1.title)        
+
+        expect(page).to have_link(@article1.title)
         expect(page).to have_link(@article2.title)
+
+        expect(page).not_to have_link("New Article")
     end
 end
